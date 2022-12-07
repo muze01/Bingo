66d2fde3fba2429f5853c9c3cb6ef51a4b691366
Paul Nimzing <muzingnimzing@gmail.com>
2022-12-07 23:17:52 +0100

    added webpack css loader

---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebPackPlugin(), new MiniCssExtractPlugin()],
+};
